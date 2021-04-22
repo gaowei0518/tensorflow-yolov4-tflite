40a7a5517f9a90044da2e3f0992596a68b46512a
gaowei <gaowei0518@gmail.com>
2021-04-22 23:39:08 +0900

    Update detect.py

--- detect.py
+++ detect.py
@@ -81,7 +81,7 @@ def main(_argv):
     image = utils.draw_bbox(original_image, pred_bbox)
     # image = utils.draw_bbox(image_data*255, pred_bbox)
     image = Image.fromarray(image.astype(np.uint8))
-    image.show()
+    #image.show()
     image = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
     cv2.imwrite(FLAGS.output, image)
 
